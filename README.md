50b74e603c198b6787a92a37ce1c331f2ff3c74a
Godwin Phillips Okpe <okpegodwin18@yahoo.com>
2025-07-03 16:59:18 +0100

    Worked on the profile app for the bootcamp stage 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# profile_app
+
+This repository contains the source code for the various projects done during the Flutter Boot Camp.
---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyProfile());
+}
+
+class MyProfile extends StatelessWidget {
+  const MyProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Bootcamp App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Roboto',
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("My Profile")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.blue, width: 3),
+              ),
+              child: ClipOval(
+                child: Image.network(
+                  'https://i.ibb.co/C3TXQrv5/My-passportpic-removebg-preview-1.png',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Passionate Software Engineer with expertise in Flutter and web development. I build scalable, user-friendly applications and enjoy mentoring the next generation of tech talent.',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontFamily: 'Roboto', fontSize: 18),
+              ),
+            ),
+            LayoutBuilder(
+              builder: (context, constraints) {
+                bool isSmallScreen = constraints.maxWidth < 600;
+                return isSmallScreen
+                    ? Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          buildSkillsSection(),
+                          SizedBox(height: 20),
+                          buildContactSection(),
+                        ],
+                      )
+                    : Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(child: buildSkillsSection()),
+                          SizedBox(width: 40),
+                          Expanded(child: buildContactSection()),
+                        ],
+                      );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildContactSection() {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Contact Information',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 4),
+        Text("Address: Makurdi, Benue State, Nigeria"),
+        Text('Email: okpephillips.dev@gmail.com'),
+        Text('Phone Number: 08122694518'),
+        Text('github: https://github.com/okpephillips'),
+      ],
+    ),
+  );
+}
+
+Widget buildSkillsSection() {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Skills', style: TextStyle(fontWeight: FontWeight.bold)),
+        SizedBox(height: 4),
+        Text("Software Engineering"),
+        Text('Mobile Development'),
+        Text('Data Analysis'),
+        Text('Monitoring and Evaluation'),
+      ],
+    ),
+  );
+}
